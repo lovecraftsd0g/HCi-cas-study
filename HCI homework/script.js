59bee72becc5b7a51e9d8251781ed0dbe6977012
lovecraftsd0g <144786427+lovecraftsd0g@users.noreply.github.com>
2023-10-24 21:48:22 +0700

    Add files via upload

---
+++ HCI homework/script.js
@@ -0,0 +1,14 @@
+function some(){
+    x1 = document.getElementById("somber1");
+    x2 = document.getElementById("somber2");
+    x3 = document.getElementById("somber3");
+    x4 = document.getElementById("somber4");
+    iiim= document.getElementById("the_im")
+    document.getElementById("haha").innerHTML = "gay"
+    iiim.style.opacity
+    if (x1 === null && x2 === null && x3 === null && x4 === null ){
+        iiim.style.opacity = "0";
+    }else if (x1 != null && x2 != null && x3 != null && x4 != null ){
+        iiim.style.opacity = "100";
+}
+}
